09804fb733cfb64132654b8ff400625d6f6908f1
Leonardo Kalyn <Leonardo.Kalyn@gmail.com>
2016-12-05 17:09:41 -0200

    Conta Corrigida

--- app/src/main/java/com/example/marge/substributaria/SubstituicaoTributaria.java
+++ app/src/main/java/com/example/marge/substributaria/SubstituicaoTributaria.java
@@ -10,7 +10,6 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class SubstituicaoTributaria extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,15 +30,125 @@ public class SubstituicaoTributaria extends AppCompatActivity {
                 EditText despesas = (EditText) findViewById(R.id.despesas_extra);
                 double extra = Double.valueOf(despesas.getText().toString());
 
-                double soma = produtos + seguro + extra;
-                double IVA = 0.1 * soma;
-                double BC_TC = soma + IVA;
-                double ICMS_ST = BC_TC;
+                if(origem == null || destino == null || produtos == 0 || seguro == 0 || despesas == 0){
+                    resultado.setText("Algum campo esta faltando");
+                }
+                else{
+                    origem = origem.toUpperCase();
+                    destino = destino.toUpperCase();
 
-                resultado.setText("Total: " + ICMS_ST);
+                    if(aliquotaPorUf(origem)==0 || aliquotaPorUf(destino)==0){
+                        resultado.setText("Origem ou Destino não é uma UF");
+                    }
+                    else{
+                        double soma = produtos + seguro + extra;
+                        double aliquota;
+                        if(origem =! destino){
+                            aliquota = aliquotaPorUf(origem);
+                        }
+                        else{
+                            aliquota = 17;
+                        }
+                        aliquota = aliquota/100;
 
+                        double MVA = (1+0.35) * (1-0.17) / (1-aliquota);
+                        double BC_TC = soma * MVA;
+
+                        double ICMS_ST = BC_TC * aliquota;
+                        resultado.setText("Total: " + ICMS_ST);
+                    }
+                }
             }
         });
 
+    }
+
+    protected int aliquotaPorUf(String uf){
+        int aliquota = 0;
+        switch (uf){
+            case "AC":
+                aliquota = 12;
+                break;
+            case "AL":
+                aliquota = 12;
+                break;
+            case "AM":
+                aliquota = 12;
+                break;
+            case "AP":
+                aliquota = 12;
+                break;
+            case "BA":
+                aliquota = 12;
+                break;
+            case "CE":
+                aliquota = 12;
+                break;
+            case "DF":
+                aliquota = 12;
+                break;
+            case "ES":
+                aliquota = 12;
+                break;
+            case "GO":
+                aliquota = 12;
+                break;
+            case "MA":
+                aliquota = 12;
+                break;
+            case "MT":
+                aliquota = 12;
+                break;
+            case "MS":
+                aliquota = 12;
+                break;
+            case "MG":
+                aliquota = 7;
+                break;
+            case "PA":
+                aliquota = 12;
+                break;
+            case "PB":
+                aliquota = 12;
+                break;
+            case "PR":
+                aliquota = 7;
+                break;
+            case "PE":
+                aliquota = 12;
+                break;
+            case "PI":
+                aliquota = 12;
+                break;
+            case "RN":
+                aliquota = 12;
+                break;
+            case "RS":
+                aliquota = 7;
+                break;
+            case "RJ":
+                aliquota = 7;
+                break;
+            case "RO":
+                aliquota = 12;
+                break;
+            case "RR":
+                aliquota = 12;
+                break;
+            case "SC":
+                aliquota = 7;
+                break;
+            case "SP":
+                aliquota = 7;
+                break;
+            case "SE":
+                aliquota = 12;
+                break;
+            case "TO":
+                aliquota = 12;
+                break;
+            default:
+                aliquota = 0;
+        }
     }
 }
